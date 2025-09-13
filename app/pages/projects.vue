380c5da028f41a9a668bcb559fcd63176afbef98
HarryHills3588 <hsh47@case.edu>
2025-09-13 17:46:11 -0400

    build: CMS for projects

--- app/pages/projects.vue
+++ app/pages/projects.vue
@@ -1,20 +1,31 @@
 <template>
-    <Fog>
-        <div>
-            <div class="p-10">
-                <h1>
-                    Completed Projects
-                </h1>
-            </div>
-            <div class="grid grid-cols-3 gap-10 pl-10 pr-10 pb-10">
-                <ProjectCard></ProjectCard>
-                <ProjectCard></ProjectCard>
-                <ProjectCard></ProjectCard>
-                <ProjectCard></ProjectCard>
-                <ProjectCard></ProjectCard>
-                <ProjectCard></ProjectCard>
-                <ProjectCard></ProjectCard>
-            </div>
-        </div>
-    </Fog>
+  <div>
+  <Fog>
+  <div class="container mx-auto px-4 py-12">
+    <h1 class="text-4xl font-bold mb-8">My Projects</h1>
+    
+    <div v-if="pending" class="text-center">Loading projects...</div>
+    
+    <div v-else-if="error" class="text-center">
+        Could not load projects.
+        <pre>
+            {{ error }}
+        </pre>
+    </div>
+
+    <div v-else class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <ProjectCard
+        v-for="project in projects"
+        :project="project"
+      />
+    </div>
+  </div>
+  </Fog>
+  </div>
 </template>
+
+<script setup lang="ts">
+    const { data: projects, pending, error } = await useAsyncData('all-projects', () =>
+      queryCollection('projects').all()
+    )
+</script>
---
+++ content/projects/project-sample.md
@@ -0,0 +1,3 @@
+# This is a project sample
+
+This is actually **working**
