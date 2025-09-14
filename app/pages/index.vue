eadcfcef2dfd6e7cf4b5d12cda4e5b16384f8333
HarryHills3588 <hsh47@case.edu>
2025-09-14 13:48:35 -0400

    build: minor changes for index - laerke

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,8 +3,8 @@
       <Fog class="h-screen">
         <UCard class="flex items-center h-1/3 bg-primary ">
             <div>
-                <h1>
-                    Hello my name is Harry Hillsdownley and it is great to have you here
+                <h1 class="text-2xl font-bold">
+                   If it ever goes down hill, I'm down
                 </h1>
             </div>
             <div>
