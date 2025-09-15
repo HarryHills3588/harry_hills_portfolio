3280b32f7a60eb7d0f6aad83cd70dbbb4889b96b
HarryHills3588 <hsh47@case.edu>
2025-09-15 10:13:50 -0400

    fix: dark theme for tailwind prose

--- app/pages/projects/[path].vue
+++ app/pages/projects/[path].vue
@@ -1,7 +1,8 @@
 <template>
     <div>
         <Fog>
-            <ProjectViewer class="mb-12 mt-12 p-6 prose max-w-none lg:w-8/12 md:w-8/12 sm:w-8/12"></ProjectViewer>
+            <ProjectViewer class="mb-12 mt-12 p-6 prose dark:prose-invert max-w-none lg:w-8/12 md:w-8/12 sm:w-8/12">
+            </ProjectViewer>
         </Fog>
     </div>
 </template>
