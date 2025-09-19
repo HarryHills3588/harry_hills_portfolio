6b829a59a10b1a8c8b79257144eb77522e3852d2
HarryHills3588 <hsh47@case.edu>
2025-09-19 16:42:47 -0400

    theme: project

--- app/components/ProjectCard.vue
+++ app/components/ProjectCard.vue
@@ -1,31 +1,22 @@
 <template>
   <UCard
-    class="group w-full max-w-sm overflow-hidden transition-all duration-300 hover:shadow-2xl hover:-translate-y-1">
-    <h2 class="text-2xl font-bold text-gray-900 dark:text-white mb-2">
+    class="group w-full max-w-sm overflow-hidden transition-all duration-300 hover:shadow-2xl hover:-translate-y-1 bg-[var(--white)]"
+    :variant="'soft'">
+    <h2 class="text-2xl font-bold text-[var(--slate-dark)] mb-2">
       {{ project.title }}
     </h2>
 
-    <p class="text-gray-600 dark:text-gray-300 mb-4 text-base leading-relaxed">
+    <p class="text-[var(--slate-dark)] text-base leading-relaxed">
       {{ project.description }}
     </p>
 
     <div class="flex flex-wrap gap-2 mb-6">
-      <UBadge
-        v-for="tag in projectTags"
-        :key="tag.name"
-        :label="tag.name"
-        :color="tag.color"
-        variant="subtle"
-        size="md"
-      ></UBadge>
+      <UBadge v-for="tag in projectTags" :key="tag.name" :label="tag.name" :color="tag.color" variant="subtle"
+        size="md"></UBadge>
     </div>
 
-    <UButton
-      label="View Project"
-      trailing-icon="i-heroicons-arrow-right-20-solid"
-      class="w-full sm:w-auto"
-      :to="`${project.path}`"
-    />
+    <UButton label="View Project" trailing-icon="i-heroicons-arrow-right-20-solid"
+      class="w-full sm:w-auto text-[var(--slate-light)]" :to="`${project.path}`" />
   </UCard>
 </template>
 
